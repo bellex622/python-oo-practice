4525364bcc8dad17953ac8c602bbfb1b055af7e8
Belle Xu <bellex22@protonmail.com>
2023-07-18 18:44:11 -0400

    finished serial generator

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,21 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    # constructor
+
+    def __init__(self, start):
+        """Create an instance with a start number"""
+        self.start = start
+        self.next_number = start
+
+    def generate(self):
+        """Increment the next_number by 1, return next_number-1"""
+        self.next_number = self.next_number + 1
+        return self.next_number - 1
+
+    def reset(self):
+        """reset the next_number to be the original number"""
+        self.next_number = self.start
+
+    def __repr__(self):
+        return f"start = {self.start} and result = {self.next_number}"
