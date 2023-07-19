b0e0e0447079325d87eba723466a8d3710ae5492
Belle Xu <bellex22@protonmail.com>
2023-07-18 20:56:09 -0400

    edit based on code review

--- wordfinder.py
+++ wordfinder.py
@@ -1,12 +1,17 @@
+import random
 class WordFinder:
-    """Word Finder: finds random words from a dictionary
+    """Word Finder: read a file with each word on a new line from the disk,
+    put every word from the file into a list, return the count of the word list
     """
     def __init__(self, file_path):
+        """call a method to put each word from the file into a list, and print
+        the length of the list"""
         self.file_path = file_path
         self.list_of_words = self.read_file_and_store_words()
         print(f"{len(self.list_of_words)} words read")
 
     def read_file_and_store_words(self):
+        """read the file, return a list of words from the file without /n"""
         f = open(f'{self.file_path}', 'r')
 
         word_list = []
@@ -16,19 +21,32 @@ class WordFinder:
 
         return word_list
 
-"""
-instantiated with path to file containing one word per line
-read file, makes attribute of list of those words
-prints out '[num-of-words-read] words read'
+    def random(self):
+        """return a random word from the word list"""
+        return random.choice(self.list_of_words)
 
-**remove \n from each word**
-(doesn't all have to be in init)
+class SpecialWordFinder(WordFinder):
+    """Extends WordFinder with a differnt method of parsing a file"""
+    def __init__(self,file_path):
+        super().__init__(file_path)
 
-has method random() that returns random word from list
-(should not re-read list each time, works with already read list)
-can use: /usr/share/dict/words
+    def read_file_and_store_words(self):
+        """Read the file,remove any empty line or comments, return the word list"""
+
+        word_list= super().read_file_and_store_words()
+        # f = open(f'{self.file_path}', 'r')
+        # word_list = []
 
 
-"""
+        # for line in f:
+        #     if line.isspace() or line[0]=="#":
+        #         continue
+        #     else:
+        #         word_list.append(line.strip())
+
+        return [word for word in word_list if not word.startswith("#") and word!=""]
+
+
+
 
 
